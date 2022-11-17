17549255a72dbb07f2d72e6b10173387353031b7
James <jamespatterson1985@yahoo.com>
2022-11-16 23:59:03 -0500

    add header, header information, links, and profile photo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,26 @@ import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <header>
+        <div class="header">
+          <h1>James Patterson | Front End Software Engineer</h1>
+          <br />
+          <h3>jamespatterson.feswe@yahoo.com</h3>
+          <br />
+          <h5>
+            <a href="https://www.linkedin.com/in/james-earl-patterson">LinkedIn</a> | 
+            <a href="https://github.com/xrzy1985">GitHub</a> | 
+            <a href="mailto: jamespatterson.feswe@yahoo.com">Email</a>
+          </h5>
+        </div>
       </header>
-    </div>
+      <div class="profile-photo">
+        <img src={require('./assets/profile.jpg')}
+              alt="A man named James smiling with short hair and a grin on his face"
+              height="200px" />
+      </div>
+    </>
   );
 }
 
