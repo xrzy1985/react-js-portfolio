ee8f3395a7f04b0ab65a8ae221d9a228d2e3e831
James <jamespatterson1985@yahoo.com>
2022-11-19 22:32:29 -0500

    add minor fixes

--- src/components/content/about/about.js
+++ src/components/content/about/about.js
@@ -35,13 +35,13 @@ export default function About(props) {
 
     return (
         <section className="about-section">
-            <h1>Greetings</h1>
+            <h1 className="header">Greetings</h1>
             <span className="paragraph">{state.greeting}</span>
-            <h1>Interests</h1>
+            <h1 className="header">Interests</h1>
             <span className="paragraph">{state.interests}</span>
-            <h1>Goals</h1>
+            <h1 className="header">Goals</h1>
             <span>{state.goals}</span>
-            <h1>Final Thoughts</h1>
+            <h1 className="header">Final Thoughts</h1>
             <span className="paragraph">{state.final}</span>
         </section>
     );
